04b50500807124e189b431bde3c2e9b1a19a2e86
balakrishnanUIDeveloper <krish.18.2009@gmail.com>
2020-06-24 15:27:04 +0530

    2 files modified, 5 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from "@angular/forms";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 import { AppComponent } from "./app.component";
+import { MailSetupComponent } from './mail-setup/mail-setup.component';
+import { DatepickerRangeComponent } from './datepicker-range/datepicker-range.component';
 // import { NgbdDatepickerRange } from "./daterange-picker/daterange-picker.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, NgbModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MailSetupComponent, DatepickerRangeComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/mail-setup/mail-setup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mail-setup',
+  templateUrl: './mail-setup.component.html',
+  styleUrls: ['./mail-setup.component.css']
+})
+export class MailSetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
