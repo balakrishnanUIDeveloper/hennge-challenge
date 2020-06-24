1a814f5b21ed997076aa70b21a34eabf0371a314
balakrishnanUIDeveloper <krish.18.2009@gmail.com>
2020-06-24 15:11:47 +0530

    1 file modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import {NgbdDatepickerRange} from './daterange-picker/daterange-picker.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule , NgbModule],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent , NgbdDatepickerRange],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
